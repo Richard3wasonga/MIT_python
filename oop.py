3108b351870b03939fb294406e9feec5e2857583
Richard Wasonga <rowwasonga@gmail.com>
2026-05-12 18:17:39 +0300

    classmethods and staticmethods

--- oop.py
+++ oop.py
@@ -16,8 +16,53 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str_1.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+
 emp_1 = Employee('Richard', 'Wasonga', 50000)
 emp_2 = Employee('Test', 'User', 60000)
+
+import datetime
+my_date = datetime.date(2016, 7, 11)
+print(Employee.is_workday(my_date))
+
+# Employee.set_raise_amt(1.05)
+
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
+
+# In this use case where someone needs to parse manually a string before making a new employee 
+
+# emp_str_1 = 'John-Doe-70000'
+# emp_str_2 = 'Steve-Smith-30000'
+# emp_str_3 = 'Jane-Doe-90000'
+
+# first, last, pay = emp_str_1.split('-')
+
+# new_emp_1 = Employee(first, last, pay)
+
+# The class method found_string return the employee object created thus using the class method as an alternative constructor
+
+# new_emp_1 = Employee.from_string(emp_str_1)
+
+# print(new_emp_1.email)
+# print(new_emp_1.pay)
+
+
 
 # Here in number of employees there is no need to use self because we don't have a use case where a instance would need it and 
 # put it under init because every time an object is created the __init__ runs
