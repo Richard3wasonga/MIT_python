ff8a675efca420a76f2cbd8d61a4607e04b21c43
Richard Wasonga <rowwasonga@gmail.com>
2026-05-14 22:48:58 +0300

    inheritance and subclasses

--- oop.py
+++ oop.py
@@ -31,13 +31,63 @@ class Employee:
             return False
         return True
 
+class Developer(Employee):
+    raise_amount = 1.10
 
-emp_1 = Employee('Richard', 'Wasonga', 50000)
-emp_2 = Employee('Test', 'User', 60000)
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
 
-import datetime
-my_date = datetime.date(2016, 7, 11)
-print(Employee.is_workday(my_date))
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+
+dev_1 = Developer('Richard', 'Wasonga', 50000, 'Python')
+dev_2 = Developer('Test', 'Employee', 60000, 'Java ')
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+
+print(mgr_1.email)
+
+mgr_1.add_emp(dev_2)
+mgr_1.remove_emp(dev_1)
+
+mgr_1.print_emps()
+
+# There are these two built in functions in python to help us know if something is an insrance or subclass
+print(isinstance(mgr_1, Manager))
+print(issubclass(Developer, Employee))
+  
+# print(help(Developer))
+
+# print(dev_1.pay)
+# dev_1.apply_raise()
+# print(dev_1.pay)
+
+# emp_1 = Employee('Richard', 'Wasonga', 50000)
+# emp_2 = Employee('Test', 'User', 60000)
+
+# import datetime
+# my_date = datetime.date(2016, 7, 11)
+# print(Employee.is_workday(my_date))
 
 # Employee.set_raise_amt(1.05)
 
