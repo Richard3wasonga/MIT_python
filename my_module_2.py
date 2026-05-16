ce1f828e5ac18e5ce6729cf46b8a135dd3065d9f
Richard Wasonga <rowwasonga@gmail.com>
2026-05-16 19:51:34 +0300

    import modules and standard library

---
+++ my_module_2.py
@@ -0,0 +1,39 @@
+# import sys
+# sys.path.append('/home/row/Development/python_DSA') # Adding this way we have to change everywhere if we change file location hence just add in python environment variable
+
+# from my_module import find_index, test
+
+# import my_module as mm
+# This imports everything from the my_module file
+# from my_module import *
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+
+# index = find_index(courses, 'Math')
+# # print(index)
+# # print(test)
+
+# print(sys.path)
+
+# import random
+# random_course = random.choice(courses)
+
+# print(random_course)
+
+# import Math
+
+# Converts 90 degrees into radians
+# rads = math.radians(90)
+
+# print(math.sin(rads))
+
+# import datetime
+# import calendar
+
+# today = datetime.date.today()
+# print(today)
+# print(calendar.isleap(2020))
+
+import os
+
+print(os.getcwd())
