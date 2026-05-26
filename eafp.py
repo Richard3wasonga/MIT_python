216069e7ec66ad018b0f1ae923a1c48f213503cc
Richard Wasonga <rowwasonga@gmail.com>
2026-05-26 16:44:24 +0300

    duck typing

---
+++ eafp.py
@@ -0,0 +1,37 @@
+# Duck Typing and Easier to ask forgiveness than permission (EAFP)
+
+
+class Duck:
+
+    def quack(self):
+        print('Quack, quack')
+
+    def fly(self):
+        print('Flap, Flap!')
+
+
+class Person:
+
+    def quack(self):
+        print("I'm Quacking Like a Duck!")
+
+    def fly(self):
+        print("I'm Flapping my Arms!")
+
+
+def quack_and_fly(thing):
+    # Not Duck-Typed (Non-Pythonic)
+    if isinstance(thing, Duck):
+        thing.quack()
+        thing.fly()
+    else:
+        print('This has to be a Duck!')
+
+    print()
+
+d = Duck()
+quack_and_fly(d)
+
+p = Person()
+quack_and_fly(p)
+
